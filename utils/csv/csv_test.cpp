53aad0bdd70fd70f4b4188fb2de9122ba4e92055
Lining Hoo <2570171677@qq.com>
2021-12-19 17:03:00 +0800

    feat: csv parser

---
+++ utils/csv/csv_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include "csv.hpp"
+
+int main() {
+    io::CSVReader<7> in("sample.csv");
+    // in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
+    int64_t timestamp, offset, len, resp_time;
+    int volume;
+    std::string host, io_type;
+    while(in.read_row(timestamp, host, volume, io_type, offset, len, resp_time)){
+        // do stuff with the data
+        std::cout << timestamp << " " << io_type << " " << offset << " " << len << std::endl;
+    }
+}
